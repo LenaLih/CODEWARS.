6e19132074ed6dae9d16b01480acf5af4614a0dd
LenaLih <lena3005@gmail.com>
2024-07-10 15:20:39 +0600

    Keep up the hoop

---
+++ Keep-up-the-hoop.js
@@ -0,0 +1,7 @@
+function hoopCount(n) { return (n >= 10) ? "Great, now move on to tricks" : "Keep at it until you get it";} 
+   //your code goes here 
+//   if(n >= 10) {
+//     return "Great, now move on to tricks";
+// } else {
+//   return "Keep at it until you get it";
+// }
