f4c98747a577a78167c1ca1dad152abaa0bb8291
LenaLih <lena3005@gmail.com>
2024-07-14 15:18:53 +0600

    Double Char

---
+++ Double-Char.js
@@ -0,0 +1,9 @@
+const doubleChar = str => str.split('').map(l => l.repeat(2)).join('');
+
+// function doubleChar(str) {
+//   let result = "";
+//   for (i = 0; i < str.length; i++){
+//     result += str[i].repeat(2);
+//   }
+//   return result;
+// }
