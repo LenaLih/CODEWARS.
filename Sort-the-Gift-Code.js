6ded0f3c98a3becb377290cbf973131d92e7acfd
LenaLih <lena3005@gmail.com>
2024-07-16 15:22:45 +0600

    Sort the Gift Code

---
+++ Sort-the-Gift-Code.js
@@ -0,0 +1,4 @@
+function sortGiftCode(code){
+   return code.split('').sort().join('');
+ }
+//  Напишите функцию с именем sortGiftCode/ sort_gift_code/ SortGiftCode, которая принимает строку, содержащую до 26 уникальных буквенных символов, и возвращает строку, содержащую те же символы в алфавитном порядке.
